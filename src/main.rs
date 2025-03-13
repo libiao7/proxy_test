4ee597ea6f968a9fc84a9b0dce3e34a244a015ee
libiao7 <31200418+libiao7@users.noreply.github.com>
2025-03-13 12:54:05 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let http_proxy = env::var("HTTP_PROXY").unwrap_or_else(|_| "未设置".to_string());
+    let https_proxy = env::var("HTTPS_PROXY").unwrap_or_else(|_| "未设置".to_string());
+
+    println!("HTTP_PROXY: {}", http_proxy);
+    println!("HTTPS_PROXY: {}", https_proxy);
 }
